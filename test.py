ae4611d94b9a13cc773f928e9cdb7e86177e455e
evuez <helloevuez@gmail.com>
2015-04-16 16:24:54 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,18 @@
+from mutations import Map
+from mutations import Body
+from mutations import EnergyBank
+
+
+def test():
+	map_ = Map(1000, 1000)
+	for i in range(20):
+		map_.add(EnergyBank(map_))
+	for i in range(5000):
+		map_.add(Body(map_))
+
+	for i in range(1000):
+		map_.tick()
+
+
+if __name__ == '__main__':
+	test()
