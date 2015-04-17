2b6ac1f28b85b7726c6fc52eabaabed73acb2a98
evuez <helloevuez@gmail.com>
2015-04-17 11:01:45 +0200

    add test_view

--- test.py
+++ test.py
@@ -1,11 +1,17 @@
+import logging
 from time import sleep
+
+from pyglet import app
+from pyglet.window import Window
+from pyglet.clock import schedule_interval
+
+from render import MapView
 from mutations import Map
 from mutations import Body
 from mutations import EnergyBank
-import logging
 
 
-logging.basicConfig(filename='test.log', level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 
 
 def test():
@@ -19,5 +25,28 @@ def test():
 		map_.tick()
 
 
+def test_view():
+	map_ = Map(500, 500)
+	for i in range(2):
+		map_.add(EnergyBank(map_))
+	for i in range(50):
+		map_.add(Body(map_))
+
+	def update(dt):
+		map_.tick()
+
+	window = Window(map_.width, map_.height)
+	map_view = MapView(map_)
+	schedule_interval(update, 0.1)
+
+	@window.event
+	def on_draw():
+		window.clear()
+		map_view.draw()
+
+
+	app.run()
+
+
 if __name__ == '__main__':
-	test()
+	test_view()
