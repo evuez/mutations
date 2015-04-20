3e02fc28d3e361f7bc378222fb7fee3a00f16f84
evuez <helloevuez@gmail.com>
2015-04-20 16:36:36 +0200

    add population graph renderer

--- render.py
+++ render.py
@@ -1,10 +1,12 @@
 import math
+from collections import deque
 from colorsys import hsv_to_rgb
 from pyglet.gl import glBegin
 from pyglet.gl import glEnd
 from pyglet.gl import glColor3f
 from pyglet.gl import glVertex2f
 from pyglet.gl import GL_TRIANGLE_FAN
+from pyglet.gl import GL_POLYGON
 from mutations import Body
 
 
@@ -21,6 +23,20 @@ def circle(cx, cy, r, color):
 	for counter in range(segments):
 		glVertex2f(x + cx, y + cy)
 		x, y = cos * x - sin * y, sin * x + cos * y
+	glEnd()
+
+
+def square(x, y, size, color):
+	size /= 2
+	x1, y1 = x - size, y - size
+	x2, y2 = x + size, y + size
+
+	glColor3f(*color)
+	glBegin(GL_POLYGON)
+	glVertex2f(x1, y1)
+	glVertex2f(x1, y2)
+	glVertex2f(x2, y2)
+	glVertex2f(x2, y1)
 	glEnd()
 
 
@@ -47,3 +63,22 @@ class MapView(object):
 					thing.RADIUS,
 					[1 * thing.energy / thing.max_energy] * 3
 				)
+
+
+class GraphView(object):
+
+	def __init__(self, map_):
+		self.map = map_
+		self.censuses = deque(maxlen=200)
+
+	def draw(self):
+		self.censuses.append(self.map.census(False))
+		max_ = max(self.censuses)
+		count = len(self.censuses)
+		for k, census in enumerate(self.censuses):
+			square(
+				k * (500 / count),
+				census * (100 / max_),
+				1,
+				(1, 1, 1)
+			)
